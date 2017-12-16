1b5ab712d15b1b077ae90b9bfc4e4f2358f3b843
liqilun <408651238@qq.com>
2017-12-16 11:09:02 +0800

    init

--- src/main/java/com/ikilun/ReflectTest.java
+++ src/main/java/com/ikilun/ReflectTest.java
@@ -17,8 +17,8 @@ public class ReflectTest {
 		movie.setName("阿凡达");
 		movie.setShowDate(new Date());
 		try {
-			Method method = movie.getClass().getDeclaredMethod("setName");
-			method.invoke("setName", "风声");
+			Method method = movie.getClass().getMethod("setName", String.class);
+			method.invoke(movie, "风声");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -31,11 +31,12 @@ public class ReflectTest {
 			//第一种方式：
 			Class clazz1 = Class.forName("com.ikilun.Movie");
 			System.out.println(clazz1);
-			//第二种方式
+			//第二种方式：任何一个类都有一个隐含的静态成员变量class
 			Class clazz2 = Movie.class;
 			System.out.println(clazz2);
-			//第三种方式（com.ikilun.Movie@15db9742 已经分配了内存，内存地址：15db9742）
+			//第三种方式（com.ikilun.Movie@15db9742 已经分配了内存，内存地址：15db9742）,已知该类的对象通过getClass方法（知道对象时用）
 			Movie movie = new Movie();
+			Class clazz3 = movie.getClass();
 			System.out.println(movie);
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
@@ -62,7 +63,12 @@ public class ReflectTest {
 			System.out.println(Modifier.toString(field.getModifiers())+" " + field.getType() + " " + field.getName());
 		}
 	}
-	
+	/*
+     * Method类，方法的对象
+	 * 一个成员方法就是一个Method对象
+	 * getMethods()方法获取的是所有的public的函数，包括父类继承而来的
+	 * getDeclaredMethods()获取的是多有该类自己声明的方法，不问访问权限
+	*/
 	public static void getClassMethod(Class clazz){
 		Method[] methods = clazz.getDeclaredMethods();
 		for(Method method : methods){
